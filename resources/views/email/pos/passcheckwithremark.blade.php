ecb365ad7fd7abdd5dd48fef529d150aa4828853
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-09-11 12:58:44 +0700

    OKAY

--- resources/views/email/pos/passcheckwithremark.blade.php
+++ resources/views/email/pos/passcheckwithremark.blade.php
@@ -29,7 +29,7 @@
                         <tbody>
                             <tr>
                                 <td style="text-align: center; padding-bottom:25px">
-                                    <img width = "120" src="{{ url('public/images/BFIE_logo') }}" alt="logo">
+                                    <img width = "120" src="{{ url('public/images/BFIE_logo.jpg') }}" alt="logo">
                                     <p style="font-size: 16px; color: #026735; padding-top: 0px;">PT. IFCA PROPERTY365 INDONESIA</p>
                                 </td>
                             </tr>
